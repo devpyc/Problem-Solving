76c3e2f059450fcfefc37bb6bbd89beb0b952d66
devpyc <136340169+devpyc@users.noreply.github.com>
2026-01-08 14:25:22 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -1,41 +1,20 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define fi first
-#define se second
-#define pb push_back
-#define pob pop_back
-#define pii pair<int,int>
-#define mi map<int,int>
-#define qi queue<int>
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define tiii tuple<int,int,int>
-#define endl "\n"
-#define io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
 int main() {
-    io;
+    cin.tie(0)->sync_with_stdio(0);
+
     int n;
-    cin >> n;
+    cin>>n;
 
-    vi dp(32,0);
+    vector<int>dp(n+1);
+    dp[2]=3;
+    dp[4]=11;
 
-    dp[0] = 1;
-    dp[1] = 0;
-    if (n >= 2) {
-        dp[2] = 3;
+    for (int i=6; i<=n; i+=2) {
+        dp[i]=dp[i-2]*4-dp[i-4];
     }
 
-    for (int i = 3; i <= n; ++i) {
-        if (i % 2 == 0) {
-            dp[i] = dp[i-2] * 3;
-            for (int j = i-4; j >= 0; j -= 2) {
-                dp[i] += dp[j] * 2;
-            }
-        }
-    }
-    cout << dp[n] << endl;
-
-    return 0;
+    cout<<dp[n];
+    // 1=0, 2=3, 3=0, 4=11, 5=0, 6=41, 7=0 ,8=153
 }
