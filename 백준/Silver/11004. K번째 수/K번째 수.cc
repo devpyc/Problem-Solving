01649ded342557231b5d28fde2bf1a30bfdcb6a3
devpyc <136340169+devpyc@users.noreply.github.com>
2026-02-06 02:59:12 +0900

    [Silver V] Title: K번째 수, Time: 468 ms, Memory: 21428 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int n,m;
-    cin>>n>>m;
+    int n,k;
+    cin>>n>>k;
+
     int arr[n];
-    for(int i=0; i<n; i++) {
+    for (int i=0; i<n; i++) {
         cin>>arr[i];
     }
-    sort(arr,arr+n);
-    cout<<arr[m-1];
+
+    nth_element(arr,arr+k-1,arr+n);
+    cout<<arr[k-1];
 }
