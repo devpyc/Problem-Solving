f424bf0374c5d8ee468f6192ce82f5f5adbbc872
devpyc <136340169+devpyc@users.noreply.github.com>
2026-02-06 02:52:18 +0900

    [Gold II] Title: 가운데를 말해요, Time: 64 ms, Memory: 8364 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -1,28 +1,29 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    priority_queue<int>maxh;
-    priority_queue<int,vector<int>,greater<int>>minh;
-
     int t;
     cin>>t;
+
+    ordered_set s;
+
+    int i=0;
     while (t--) {
         int x;
         cin>>x;
 
-        if (maxh.empty()||x<=maxh.top()) maxh.push(x);
-        else minh.push(x);
-
-        if (maxh.size()>minh.size()+1) {
-            minh.push(maxh.top());
-            maxh.pop();
-        }else if (minh.size()>maxh.size()) {
-            maxh.push(minh.top());
-            minh.pop();
+        s.insert({x,i++});
+        if (s.size()%2==0) {
+            cout<<s.find_by_order(s.size()/2-1)->first<<"\n";
+        }else {
+            cout<<s.find_by_order(s.size()/2)->first<<"\n";
         }
-        cout<<maxh.top()<<"\n";
     }
 }
