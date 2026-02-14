370a4af62c1cd014833c6c955b9a724aa0143fba
devpyc <136340169+devpyc@users.noreply.github.com>
2026-02-14 21:33:18 +0900

    [Bronze II] Title: 막대기, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Bronze/17608. 막대기/막대기.cc
+++ 백준/Bronze/17608. 막대기/막대기.cc
@@ -1,19 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 int main() {
-    int n;
-    cin>>n;
-    int st[n];
-    for(int i=0; i<n; i++){
-        cin>>st[i];
+    cin.tie(0)->sync_with_stdio(0);
+
+    int t;
+    cin>>t;
+    stack<int>st;
+    while (t--) {
+        int x;
+        cin>>x;
+        st.push(x);
     }
-    int max=0;
-    int count=0;
-    for(int i=n-1; i>=0; i--){
-        if(st[i]>max){
-            count++;
-            max=st[i];
+    int a=st.top();
+    st.pop();
+    int cnt=1;
+    while (!st.empty()) {
+        if (st.top()>a) {
+            a=st.top();
+            cnt++;
+            st.pop();
+        }else {
+            st.pop();
         }
     }
-    cout<<count;
+    cout<<cnt;
 }
