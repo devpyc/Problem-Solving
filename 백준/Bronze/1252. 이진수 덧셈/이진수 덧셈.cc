e3e8180680194932eeecc50caec925a4acee1562
devpyc <136340169+devpyc@users.noreply.github.com>
2026-02-14 20:43:46 +0900

    [Bronze II] Title: 이진수 덧셈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.cc
+++ 백준/Bronze/1252. 이진수 덧셈/이진수 덧셈.cc
@@ -1,65 +1,19 @@
 #include <bits/stdc++.h>
-#define int long long
-#define fi first
-#define se second
-#define pb push_back
-#define as assign
-#define pi pair<int,int>
-#define mi map<int,int>
-#define qi queue<int>
-#define vi vector<int>
-#define vvi vector<vector<int>>
-#define endl "\n"
-#define io ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
-const int MAX = LLONG_MAX;
 
-void pad(string &a, string &b) {
-    if (a.length() < b.length()) swap(a, b);
-    while (a.length() != b.length()) b = '0' + b;
-}
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-string add(const string &a, const string &b) {
-    string result = "";
-    bool carry = false;
-    for (int i = a.length() - 1; i >= 0; --i) {
-        int t1 = a[i] == '1' ? 1 : 0;
-        int t2 = b[i] == '1' ? 1 : 0;
-        int sum = t1 + t2 + carry;
+    string a,b;
+    cin>>a>>b;
 
-        if (sum == 3) {
-            result = '1' + result;
-            carry = true;
-        } else if (sum == 2) {
-            result = '0' + result;
-            carry = true;
-        } else if (sum == 1) {
-            result = '1' + result;
-            carry = false;
-        } else {
-            result = '0' + result;
-            carry = false;
-        }
+    bitset<81>A(a),B(b);
+    while (B.any()) {
+        bitset<81>c=A&B;
+        A^=B;
+        B=c<<1;
     }
-
-    if (carry) result = '1' + result;
-    return result;
-}
-
-string solve(const string &result) {
-    size_t first_one = result.find_first_not_of('0');
-    if (first_one == string::npos) return "0";
-    return result.substr(first_one);
-}
-
-int32_t main() {
-    string a, b;
-    cin >> a >> b;
-
-    pad(a, b);
-    string res = add(a, b);
-    string ans = solve(res);
-
-    cout << ans << endl;
-    return 0;
+    string res=A.to_string();
+    size_t check=res.find('1');
+    cout<<(check==string::npos?"0":res.substr(check));
 }
