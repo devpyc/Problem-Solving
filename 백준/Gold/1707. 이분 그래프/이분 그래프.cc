c4bad152d120f3e2f5b6f06f6bdb0c2d13d7b72d
devpyc <136340169+devpyc@users.noreply.github.com>
2026-02-14 21:41:03 +0900

    [Gold IV] Title: 이분 그래프, Time: 168 ms, Memory: 2384 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,50 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int>parent;
+
+int find(int x) {
+    if (parent[x]==x) return x;
+    else return parent[x]=find(parent[x]);
+}
+
+void unite(int x, int y) {
+    x=find(x);
+    y=find(y);
+    if (x!=y) parent[y]=x;
+}
+
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
     int t;
-    cin >> t;
-    while(t--) {
-        int v, e;
-        cin >> v >> e;
+    cin>>t;
+    while (t--) {
+        vector<pair<int,int>>arr;
+        int v,e;
+        cin>>v>>e;
+        parent.resize(2*v+1);
+        iota(parent.begin(),parent.end(),0);
 
-        vector<vector<int>> graph(v + 1);
-        for (int i = 0; i < e; i++) {
-            int u, v;
-            cin >> u >> v;
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        bool check=true;
+        while (e--) {
+            int a,b;
+            cin>>a>>b;
+            if (find(a)==find(b)) check=false;
+            unite(a,b+v);
+            unite(b,a+v);
         }
-
-        vector<int> arr(v + 1, 0);
-        bool check = true;
-
-        for (int i = 1; i <= v && check; i++) {
-            if (arr[i] == 0) {
-                queue<int> q;
-                q.push(i);
-                arr[i] = 1;
-
-                while(!q.empty() && check) {
-                    int cur = q.front();
-                    q.pop();
-
-                    for (int next : graph[cur]) {
-                        if (arr[next] == 0) {
-                            arr[next] = -arr[cur];
-                            q.push(next);
-                        } else if (arr[next] == arr[cur]) {
-                            check = false;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        cout << (check ? "YES" : "NO") << "\n";
+        cout<<(check?"YES\n":"NO\n");
     }
 }
