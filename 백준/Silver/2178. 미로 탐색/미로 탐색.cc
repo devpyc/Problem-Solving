4e71a8cdc10b0300e246ef1b6f8e17606ad50442
devpyc <136340169+devpyc@users.noreply.github.com>
2026-02-20 12:14:20 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2248 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int n, m;
-int arr[101][101];
-int visited[101][101];
-int dx[] = {0, 0, 1, -1};
-int dy[] = {1, -1, 0, 0};
+int n,m;
+int board[101][101];
+int dist[101][101];
+bool visited[101][101];
 
-void bfs(int x, int y) {
-    queue<pair<int, int>> q;
-    q.push({x, y});
-    visited[x][y] = 1;
+int dx[4]={-1,1,0,0};
+int dy[4]={0,0,-1,1};
+
+void bfs(int sx, int sy) {
+    queue<pair<int,int>>q;
+    q.push({sx,sy});
+
+    visited[sx][sy]=true;
+    dist[sx][sy]=1;
+
     while (!q.empty()) {
-        int cx = q.front().first;
-        int cy = q.front().second;
+        int x=q.front().first;
+        int y=q.front().second;
+
         q.pop();
-        for (int i = 0; i < 4; i++) {
-            int nx = cx + dx[i];
-            int ny = cy + dy[i];
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-                if (arr[nx][ny] == 1 && visited[nx][ny] == 0) {
-                    q.push({nx, ny});
-                    visited[nx][ny] = visited[cx][cy] + 1;
+
+        for (int i=0; i<4; i++) {
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+
+            if (nx>=1&&nx<=n&&ny>=1&&ny<=m) {
+                if (!visited[nx][ny]&&board[nx][ny]==1) {
+                    visited[nx][ny]=true;
+                    dist[nx][ny]=dist[x][y]+1;
+                    q.push({nx,ny});
                 }
             }
         }
@@ -30,14 +38,20 @@ void bfs(int x, int y) {
 }
 
 int main() {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
+    cin.tie(0)->sync_with_stdio(0);
+
+    cin>>n>>m;
+
+    for (int i=1; i<=n; i++) {
         string s;
-        cin >> s;
-        for (int j = 0; j < m; j++) {
-            arr[i][j] = s[j] - '0';
+        cin>>s;
+
+        for (int j=1; j<=m; j++) {
+            board[i][j]=s[j-1]-'0';
         }
     }
-    bfs(0, 0);
-    cout << visited[n - 1][m - 1];
+
+    bfs(1,1);
+    
+    cout<<dist[n][m];
 }
