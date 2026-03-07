b6b9377af9a69e68d7504253bc4726a64dd7255f
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-07 12:49:04 +0900

    [Gold II] Title: 가운데를 말해요, Time: 80 ms, Memory: 8364 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 
-typedef tree<pair<int,int>, null_type, less<pair<int,int>>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+using ordered_set=tree<pair<int,int>,null_type,less<pair<int,int>>,rb_tree_tag,tree_order_statistics_node_update>;
 
 int main() {
     cin.tie(0)->sync_with_stdio(0);
@@ -14,16 +14,11 @@ int main() {
 
     ordered_set s;
 
-    int i=0;
-    while (t--) {
+    for (int i=0; i<t; i++) {
         int x;
         cin>>x;
 
-        s.insert({x,i++});
-        if (s.size()%2==0) {
-            cout<<s.find_by_order(s.size()/2-1)->first<<"\n";
-        }else {
-            cout<<s.find_by_order(s.size()/2)->first<<"\n";
-        }
+        s.insert({x,i});
+        cout<<(s.size()%2==0?s.find_by_order(s.size()/2-1)->first:s.find_by_order(s.size()/2)->first)<<"\n";
     }
 }
