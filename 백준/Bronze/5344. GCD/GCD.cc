e38415fc23eaa13f2b0fb474004824b8000c2fc7
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-08 21:49:03 +0900

    [Bronze I] Title: GCD, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5344. GCD/GCD.cc
+++ 백준/Bronze/5344. GCD/GCD.cc
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     cin.tie(0)->sync_with_stdio(0);
 
     int t;
@@ -9,6 +9,6 @@ int main(){
     while (t--) {
         int a,b;
         cin>>a>>b;
-        cout<<gcd(a,b)<<"\n";
+        cout<<std::gcd(a,b)<<"\n";
     }
 }
