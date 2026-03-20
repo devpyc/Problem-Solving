1e216383c1e0601471383964e747d58e9812123b
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-21 00:50:59 +0900

    [Gold V] Title: AC, Time: 32 ms, Memory: 3076 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,71 +1,62 @@
-#include <iostream>
-#include <deque>
-#include <string>
-
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    int T;
-    cin >> T;
+    cin.tie(0)->sync_with_stdio(0);
 
-    while (T--) {
-        string p;
-        cin >> p;
+    int t;
+    cin>>t;
+
+    while (t--) {
+        string s;
+        cin>>s;
 
         int n;
-        cin >> n;
+        cin>>n;
+        string x;
+        cin>>x;
+        deque<int>arr;
+        int num=0;
+        bool check = false;
 
-        deque<int> dq;
-        string arr;
-        cin >> arr;
-
-        int num = 0;
-        for (char c : arr) {
+        for (char c:x) {
             if (isdigit(c)) {
-                num = num * 10 + (c - '0');
-            } else if (num > 0) {
-                dq.push_back(num);
-                num = 0;
+                num=num*10+(c-'0');
+                check=true;
+            } else {
+                if (check) {
+                    arr.push_back(num);
+                    num=0;
+                    check=false;
+                }
             }
         }
-        bool reverse = false;
-        bool error = false;
-
-        for (char c : p) {
-            if (c == 'R') {
-                reverse = !reverse;
-            } else if (c == 'D') {
-                if (dq.empty()) {
-                    error = true;
+        bool reverse=false;
+        bool check2=true;
+        for (int i=0; i<s.size(); i++) {
+            if (s[i]=='R') {
+                reverse=!reverse;
+            }else {
+                if (arr.empty()) {
+                    cout<<"error\n";
+                    check2=false;
                     break;
                 }
-
-                if (reverse) {
-                    dq.pop_back();
-                } else {
-                    dq.pop_front();
-                }
+                if (!reverse) arr.pop_front();
+                else arr.pop_back();
             }
         }
-        if (error) {
-            cout << "error\n";
-        } else {
-            cout << "[";
-            while (!dq.empty()) {
-                if (reverse) {
-                    cout << dq.back();
-                    dq.pop_back();
-                } else {
-                    cout << dq.front();
-                    dq.pop_front();
-                }
-
-                if (!dq.empty()) {
-                    cout << ",";
-                }
+        if (!check2) continue;
+        cout<<"[";
+        while (!arr.empty()) {
+            if (!reverse) {
+                cout<<arr.front();
+                arr.pop_front();
+            }else {
+                cout<<arr.back();
+                arr.pop_back();
             }
-            cout << "]\n";
+            if (!arr.empty()) cout<<",";
         }
+        if (check2) cout<<"]\n";
     }
-    return 0;
 }
