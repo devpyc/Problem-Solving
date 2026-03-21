559baa07bab914b5805b4fbcebe920efe1657a68
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-21 12:00:20 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.cc
+++ 백준/Bronze/8393. 합/합.cc
@@ -1,14 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    cin>>n;
-    int ans=0;
-    for(int i=1; i<=n; i++) {
-        ans+=i;
-    }
-    cout<<ans;
-}
+#include <iostream>
+int main(){int n;std::cin>>n;std::cout<<n*(n+1)/2;}
