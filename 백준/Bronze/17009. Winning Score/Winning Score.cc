105815e00b0f504bf3953aff33b65da5fc12b6c6
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-24 14:47:22 +0900

    [Bronze IV] Title: Winning Score, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/17009. Winning Score/Winning Score.cc
+++ 백준/Bronze/17009. Winning Score/Winning Score.cc
@@ -1,23 +1,7 @@
-#include <bits/stdc++.h>
-#define ll long long
-using namespace std;
-
+#include <iostream>
 int main() {
-    int A[3], B[3];
-    for (int i = 0; i < 3; i++) {
-        cin >> A[i];
-    }
-    for (int i = 0; i < 3; i++) {
-        cin >> B[i];
-    }
-    int a = A[0] * 3 + A[1] * 2 + A[2];
-    int b = B[0] * 3 + B[1] * 2 + B[2];
-
-    if (a > b) {
-        cout << "A";
-    } else if (a < b) {
-        cout << "B";
-    } else {
-        cout << "T";
-    }
+    int a,b,c,d,e,f;
+    std::cin>>a>>b>>c>>d>>e>>f;
+    int A=a*3+b*2+c,B=d*3+e*2+f;
+    std::cout<<(A>B?'A':(A==B?'T':'B'));
 }
