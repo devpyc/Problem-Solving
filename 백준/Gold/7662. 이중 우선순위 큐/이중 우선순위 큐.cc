fdeed91e1515afcf892000daaafa67c0e9848778
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-26 14:58:41 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1856 ms, Memory: 64596 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
 
-template <typename T>
+template<typename T>
 using ordered_set=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 
 int main() {
@@ -11,34 +12,33 @@ int main() {
 
     int t;
     cin>>t;
-    while (t--) {
-        int n;
-        cin>>n;
-        ordered_set<pair<int,int>>s;
+    while (t--){
+        int q;
+        cin>>q;
+        ordered_set<pair<int,int>>os;
         int i=0;
-        while (n--) {
+        while (q--) {
             char c;
             cin>>c;
             if (c=='I') {
                 int x;
                 cin>>x;
-                s.insert({x,i++});
+                os.insert({x,i++});
             }else {
                 int x;
                 cin>>x;
-                if (!s.empty()) {
-                    if (x==-1) {
-                        s.erase(s.find_by_order(0));
-                    }else {
-                        s.erase(s.find_by_order(s.size()-1));
-                    }
+                if (x==1) {
+                    os.erase(os.find_by_order(os.size()-1));
+                }else {
+                    os.erase(os.find_by_order(0));
                 }
             }
         }
-        if (!s.empty()) {
-            cout<<s.find_by_order(s.size()-1)->first<<" "<<s.find_by_order(0)->first<<"\n";
+        if (!os.empty()) {
+            cout<<os.find_by_order(os.size()-1)->first<<" "<<os.find_by_order(0)->first<<"\n";
         }else {
             cout<<"EMPTY\n";
         }
+
     }
 }
