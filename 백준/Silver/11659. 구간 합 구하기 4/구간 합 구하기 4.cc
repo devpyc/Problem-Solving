008f989adb91f48f1d3f61d7fb69a479e15c21d6
devpyc <136340169+devpyc@users.noreply.github.com>
2026-03-30 16:07:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 40 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,21 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+long long sum(vector<long long>&tree,int i) {
+    long long ans=0;
+    while (i>0) {
+        ans+=tree[i];
+        i-=(i&-i);
+    }
+    return ans;
+}
+
+void update(vector<long long>&tree, int i, long long diff) {
+    while (i<tree.size()) {
+        tree[i]+=diff;
+        i+=(i&-i);
+    }
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
 
     int n,m;
     cin>>n>>m;
-    int ans[100001]={0,};
-    for(int i=1; i<=n; i++) {
-        int x;
-        cin>>x;
-        ans[i]=ans[i-1]+x;
+
+    vector<long long>arr(n+1);
+    vector<long long>tree(n+1);
+    for (int i=1; i<=n; i++) {
+        cin>>arr[i];
+        update(tree,i,arr[i]);
     }
-    while(m--) {
-        int a,b;
-        cin>>a>>b;
-        cout<<ans[b]-ans[a-1]<<"\n";
+    while (m--) {
+        int i,j;
+        cin>>i>>j;
+        cout<<sum(tree,j)-sum(tree,i-1)<<"\n";
     }
 }
