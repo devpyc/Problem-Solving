591bfe0eceed66ee953a04aea2ed353a25982279
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-01 14:35:59 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.cc
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.cc
@@ -4,22 +4,21 @@ using namespace std;
 int main() {
     cin.tie(0)->sync_with_stdio(0);
 
-    int n,m;
-    cin>>n>>m;
-    long long arr[n];
+    int n,x;
+    cin>>n>>x;
+
+    vector<long long>arr(n);
     long long sum=0;
     for (int i=0; i<n; i++) {
         cin>>arr[i];
-        if (i<m) {
+        if (i<x) {
             sum+=arr[i];
         }
     }
-    long long M=sum;
-    for (int i=m; i<n; i++) {
-        sum+=arr[i]-arr[i-m];
-        if (M<sum) {
-            M=sum;
-        }
+    long long ans=sum;
+    for (int i=x; i<n; i++) {
+        sum+=arr[i]-arr[i-x];
+        ans=max(ans,sum);
     }
-    cout<<M;
+    cout<<ans;
 }
