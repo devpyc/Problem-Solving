c9c61474b35e301c09b81ae64f9df738083cd64f
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-14 16:01:31 +0900

    [Gold II] Title: 중앙값 구하기, Time: 4 ms, Memory: 2688 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.cc
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.cc
@@ -1,50 +1,29 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+template <typename T>
+using ordered_set=tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
 
     int t;
     cin>>t;
-
-    while(t--) {
-        int m;
-        cin>>m;
-
-        int cnt=(m+1)/2;
-        cout<<cnt<<"\n";
-
-        priority_queue<int>maxh;
-        priority_queue<int,vector<int>,greater<int>>minh;
-
-        int num;
-        int out=0;
-
-        for(int i=0; i<m; i++) {
-            cin>>num;
-
-            if (maxh.empty() || num <= maxh.top()) {
-                maxh.push(num);
-            } else {
-                minh.push(num);
-            }
-
-            if (maxh.size() > minh.size() + 1) {
-                minh.push(maxh.top());
-                maxh.pop();
-            } else if (minh.size() > maxh.size()) {
-                maxh.push(minh.top());
-                minh.pop();
-            }
-
-            if(i%2==0) {
-                cout<<maxh.top();
-                out++;
-
-                if(out%10==0||out==cnt) cout<<"\n";
-                else cout<<" ";
-            }
+    while (t--) {
+        ordered_set<pair<int,int>>s;
+        int n;
+        cin>>n;
+        cout<<n/2+1<<"\n";
+        for (int i=1; i<=n; i++) {
+            int x;
+            cin>>x;
+            s.insert({x,i});
+            if (i%2==1) cout<<s.find_by_order(s.size()/2)->first<<" ";
         }
+        cout<<"\n";
     }
 }
