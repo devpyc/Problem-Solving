bf5b00f18f2e04f220294338f1f6bd25015434e9
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-14 14:25:12 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 4 ms, Memory: 3544 KB -BaekjoonHub

--- 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
+++ 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.cc
@@ -1,50 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+vector<int>manacher(const string &ans) {
+    int n=ans.size()*2+1;
+    vector<int>res(n);
+    string s="#";
+    for (auto i:ans) s+=i,s+="#";
+    for (int i=0,p=-1,r=-1; i<n; i++) {
+        res[i]=i<=r?min(r-i,res[2*p-i]):0;
+        while (i-res[i]-1>=0&&i+res[i]+1<n&&s[i-res[i]-1]==s[i+res[i]+1]) res[i]++;
+        if (i+res[i]>r) r=i+res[i],p=i;
+    }
+    return res;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
     string s;
-    cin >> s;
-
-    string t = "#";
-    for(char c : s){
-        t += c;
-        t += '#';
-    }
-    int n = t.length();
-    vector<int> p(n, 0);
-    int c = 0, r = 0;
-
-    for(int i=0; i<n; ++i){
-        int mirr = 2*c - i;
-
-        if(i < r){
-            p[i] = min(r - i, p[mirr]);
-        }
-
-        int a = i + p[i] + 1;
-        int b = i - p[i] - 1;
-
-        while(a < n && b >=0 && t[a] == t[b]){
-            p[i]++;
-            a++;
-            b--;
-        }
-
-        if(i + p[i] > r){
-            c = i;
-            r = i + p[i];
-        }
-    }
-
-    int ans = 0;
-
-    for(int len : p){
-        if(len > ans){
-            ans = len;
-        }
-    }
-    cout << ans;
+    cin>>s;
+    auto ans=manacher(s);
+    cout<<*max_element(ans.begin(),ans.end());
 }
