815df81dacab861fe2ab4fb84c5309d479da16db
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-17 00:33:18 +0900

    [Gold I] Title: 수들의 합 7, Time: 416 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Gold/2268. 수들의 합 7/수들의 합 7.cc
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.cc
@@ -1,92 +1,41 @@
 #include <bits/stdc++.h>
-#define ll long long
-#define endl "\n"
 using namespace std;
 
-class SegmentTree {
-private:
-    vector<ll> tree;
-    vector<ll> arr;
-    int size;
-
-    ll build(int node, int start, int end) {
-        if (start == end) {
-            return tree[node] = arr[start];
-        }
-        int mid = (start + end) / 2;
-        ll left_sum = build(2 * node, start, mid);
-        ll right_sum = build(2 * node + 1, mid + 1, end);
-        return tree[node] = left_sum + right_sum;
+long long sum(vector<long long>&tree, int i) {
+    long long ans=0;
+    while (i>0) {
+        ans+=tree[i];
+        i-=(i&-i);
     }
+    return ans;
+}
 
-    ll query(int node, int start, int end, int L, int R) {
-        if (R < start || end < L) {
-            return 0;
-        }
-        if (L <= start && end <= R) {
-            return tree[node];
-        }
-        int mid = (start + end) / 2;
-        ll left_sum = query(2 * node, start, mid, L, R);
-        ll right_sum = query(2 * node + 1, mid + 1, end, L, R);
-        return left_sum + right_sum;
+void update(vector<long long>&tree, int i, long long diff) {
+    while (tree.size()>i) {
+        tree[i]+=diff;
+        i+=(i&-i);
     }
-
-    void update(int node, int start, int end, int idx, ll value) {
-        if (start == end) {
-            tree[node] = value;
-        } else {
-            int mid = (start + end) / 2;
-            if (start <= idx && idx <= mid) {
-                update(2 * node, start, mid, idx, value);
-            } else {
-                update(2 * node + 1, mid + 1, end, idx, value);
-            }
-            tree[node] = tree[2 * node] + tree[2 * node + 1];
-        }
-    }
-
-public:
-    SegmentTree(int n) {
-        size = n;
-        tree.resize(4 * n);
-        arr.resize(n + 1);
-    }
-
-    void build() {
-        build(1, 1, size);
-    }
-
-    ll query(int L, int R) {
-        if (L > R) {
-            swap(L, R);
-        }
-        return query(1, 1, size, L, R);
-    }
-
-    void update(int idx, ll value) {
-        update(1, 1, size, idx, value);
-    }
-};
+}
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
 
-    int N, M;
-    cin >> N >> M;
+    int n,m;
+    cin>>n>>m;
 
-    SegmentTree segTree(N);
+    vector<long long>tree(n+1),arr(n+1);
 
-    for (int i = 0; i < M; ++i) {
-        int cmd, x, y;
-        cin >> cmd >> x >> y;
-        if (cmd == 0) {
-            cout << segTree.query(x, y) << endl;
-        } else if (cmd == 1) {
-            segTree.update(x, y);
+    while (m--) {
+        long long a,i,j;
+        cin>>a>>i>>j;
+
+        if (a==0) {
+            if (i>j) swap(i,j);
+            cout<<sum(tree,j)-sum(tree,i-1)<<"\n";
+        }else {
+            long long diff=j-arr[i];
+            arr[i]=j;
+            update(tree,i,diff);
         }
     }
-
-    return 0;
 }
