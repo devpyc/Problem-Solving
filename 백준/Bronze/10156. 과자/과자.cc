26b90c455f4bbad67cfea9ccc89edbcecdc9075d
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-17 00:40:32 +0900

    [Bronze IV] Title: 과자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10156. 과자/과자.cc
+++ 백준/Bronze/10156. 과자/과자.cc
@@ -1,11 +1,2 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cin.tie(0)->sync_with_stdio(0);
-
-    int a,b,c;
-    cin>>a>>b>>c;
-    int s=a*b-c;
-    cout<<(s<0?0:s);
-}
+main(){int a,b,c,s;std::cin>>a>>b>>c;s=a*b-c;std::cout<<(s>=0)*s;}
