288ecd6a26b557ed2f44d235cdbaf8b00afb4792
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-17 00:33:06 +0900

    [Bronze II] Title: 숫자, Time: 12 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.cc
+++ 백준/Bronze/10093. 숫자/숫자.cc
@@ -1,27 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main() {
-    long long A, B;
+    cin.tie(0)->sync_with_stdio(0);
 
-    cin >> A >> B;
-
-    if (A > B) {
-        long long temp = A;
-        A = B;
-        B = temp;
-    }
-
-    if (A >= B || B - A == 1) {
-        cout << 0 << endl;
+    long long a,b;
+    cin>>a>>b;
+    if (a==b) {
+        cout<<0;
         return 0;
     }
+    if (a>b) swap(a,b);
 
-    cout << B - A - 1 << endl;
-
-    for (long long i = A + 1; i < B; i++) {
-        cout << i << " ";
+    cout<<b-a-1<<"\n";
+    for (long long i=a+1; i<b; i++) {
+        cout<<i<<" ";
     }
-
-    return 0;
 }
