a893cdf2359a3da4bdfe44e2ae31b0a80f4e6abf
devpyc <136340169+devpyc@users.noreply.github.com>
2026-04-18 22:01:49 +0900

    [Silver IV] Title: 最大の和, Time: 8 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/5591. 最大の和/最大の和.cc
+++ 백준/Silver/5591. 最大の和/最大の和.cc
@@ -6,18 +6,20 @@ int main() {
 
     int n,m;
     cin>>n>>m;
-    int arr[n];
+
+    vector<long long>arr(n);
     long long sum=0;
     for (int i=0; i<n; i++) {
         cin>>arr[i];
-        if (i<m) sum+=arr[i];
-    }
-    long long M=sum;
-    for (int i=m; i<n; i++) {
-        sum+=arr[i]-arr[i-m];
-        if (M<sum) {
-            M=sum;
+        if (i<m) {
+            sum+=arr[i];
         }
     }
-    cout<<M;
+
+    long long Max=sum;
+    for (int i=m; i<n; i++) {
+        sum+=arr[i]-arr[i-m];
+        Max=max(Max,sum);
+    }
+    cout<<Max;
 }
